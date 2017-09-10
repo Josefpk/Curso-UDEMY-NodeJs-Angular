c69d86f071f5d9ac3603721b51bed752d369646d
Josefpk <josef.ban.v@gmail.com>
2017-09-10 15:19:02 -0500

    Aqui creamos un demonio para que cada vez que se ejecute un cambio en el script lo valla ejecunatndo en el node js

--- api-restful-favoritos/index.js
+++ api-restful-favoritos/index.js
@@ -2,10 +2,18 @@
 
 const express = require('express');
 
+//BODY PARSER, convierto todo lo que llegue como parametro a JSON
+const bodyParser = require('body-parser');
+
 var app = express();
+var port = process.env.PORT || 3678;
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
 
 // aqui se crea el servidor... se pasa como primer parametro el pueto
 // Segundo parametro recibe una funcion de Calback
-app.listen(3678, function(){
-	console.log('API REST FAVORITOS FUNCIONANDO EN http://localhost:3678');
+app.listen(port, function(){
+	console.log(`API REST FAVORITOS FUNCIONANDO EN http://localhost:${port}`);
+	console.log('Prueba demonio');
 });	
