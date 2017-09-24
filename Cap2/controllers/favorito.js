f98a8a235c14ecee0650c00e572810894976eca8
Josefpk <josef.ban.v@gmail.com>
2017-09-24 16:00:50 -0500

    Acciones de un controlador, metodos GET, POST, SAVE, UPDATE, DELETE

--- Cap2/controllers/favorito.js
+++ Cap2/controllers/favorito.js
@@ -12,6 +12,43 @@ function prueba(req, res) {
 	res.send({texto: "Hola mundo con NodeJs y EXPRESS - "+nombre})
 }
 
+function getFavorito(req, res){
+	var favoritoId= req.params.Id;
+
+	res.status(200).send({data: favoritoId});
+}
+
+function getFavoritos(req, res){
+	
+}
+
+function saveFavorito(req, res){
+	var params = req.body; 
+
+	res.status(200).send({favorito: params});
+}
+
+function updateFavorito(req, res){
+	var params = req.body; 
+
+	res.status(200).send({update: true, favorito: params});
+
+}
+
+function deleteFavorito(req, res){
+	var favoritoId= req.params.Id;
+
+	res.status(200).send({delete: true, data: favoritoId});
+}
+
+
+
+
 module.exports = {
-	prueba
+	prueba,
+	getFavorito,
+	saveFavorito,
+	updateFavorito,
+	deleteFavorito,
+	getFavoritos
 }
--- Cap2/routes/favorito.js
+++ Cap2/routes/favorito.js
@@ -4,6 +4,11 @@ var express = require('express');
 var FavoritoController = require('../controllers/favorito');
 var api = express.Router();
 
+
 api.get('/prueba/:nombre?', FavoritoController.prueba);
+api.get('/favorito/:Id?', FavoritoController.getFavorito);
+api.post('/favorito', FavoritoController.saveFavorito);
+api.put('/favorito', FavoritoController.updateFavorito);
+api.delete('/favorito/:Id', FavoritoController.deleteFavorito);
 
 module.exports = api;
